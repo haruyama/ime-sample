cd02ae82bee9a98c20e10cf5afc97bde1aa3137d
HARUYAMA Seigo <haruyama@unixuser.org>
2012-02-18 23:38:56 +0900

    add common.dic/common-prefix-search.

--- train/src/common/dic.clj
+++ train/src/common/dic.clj
@@ -3,8 +3,9 @@
 (require '[clojure.contrib.string :as string])
 
 
-(defn init [filename]
+(defn init []
   (let [ht (new java.util.HashMap)]
+
     (defn add [r w]
       (if (.containsKey ht r)
         (.add (.get ht r) w)
@@ -12,15 +13,29 @@
           (let [s (new java.util.HashSet)]
             (.add s w)
             (.put ht r s)))))
+
     (defn find- [s]
       (if (.containsKey ht s)
         (.get ht s)
         java.util.Collections/EMPTY_SET))
-    (let [func {:add add :find find-}]
-      (with-open [fr (new java.io.FileReader filename)
-                  br (new java.io.BufferedReader fr)]
-        (doseq [line (take-while (comp not nil?) (repeatedly #(.readLine br)))]
-          (let [row (string/split #"\t" line)]
-            (add (str (first row)) (str (nth row 1))))))
-      func)))
+
+    (defn common-prefix-search [s, m]
+      (let [result (new java.util.ArrayList)]
+        (doseq [i (range 1 (+ 1 (min m (count s))))]
+          (let [r (subs s 0 i)]
+            (doseq [w (find- s)]
+              (.add result [r w])
+              )
+            )
+          )
+        result)
+      )
+
+    (defn add-line [l]
+      (let [row (string/split #"\t" l)]
+        (add (str (first row)) (str (nth row 1)))))
+
+    {:add add :find find- :common_prefix_search common-prefix-search :add_line add-line}
+    )
+  )
 
