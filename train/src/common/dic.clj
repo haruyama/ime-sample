ff021a2c3290023e5d2f89e89f2f5246aa6cac59
HARUYAMA Seigo <haruyama@unixuser.org>
2012-02-18 22:37:25 +0900

    use closures.

--- train/src/common/dic.clj
+++ train/src/common/dic.clj
@@ -2,25 +2,25 @@
 
 (require '[clojure.contrib.string :as string])
 
-(def ht (new java.util.HashMap))
-
-
-(defn- add [r w]
-  (if (.containsKey ht r)
-    (.add (.get ht r) w)
-    (do
-      (let [s (new java.util.HashSet)]
-        (.add s w)
-        (.put ht r s)))))
-
-(defn f [s]
-  (if (.containsKey ht s)
-    (.get ht s)
-    java.util.Collections/EMPTY_SET))
 
 (defn init [filename]
-  (with-open [fr (new java.io.FileReader filename)
-              br (new java.io.BufferedReader fr)]
-    (doseq [line (take-while (comp not nil?) (repeatedly #(.readLine br)))]
-      (let [row (string/split #"\t" line)]
-        (add (str (first row)) (str (nth row 1)))))))
+  (let [ht (new java.util.HashMap)]
+    (defn add [r w]
+      (if (.containsKey ht r)
+        (.add (.get ht r) w)
+        (do
+          (let [s (new java.util.HashSet)]
+            (.add s w)
+            (.put ht r s)))))
+    (defn find- [s]
+      (if (.containsKey ht s)
+        (.get ht s)
+        java.util.Collections/EMPTY_SET))
+    (let [func {:add add :find find-}]
+      (with-open [fr (new java.io.FileReader filename)
+                  br (new java.io.BufferedReader fr)]
+        (doseq [line (take-while (comp not nil?) (repeatedly #(.readLine br)))]
+          (let [row (string/split #"\t" line)]
+            (add (str (first row)) (str (nth row 1))))))
+      func)))
+
--- train/src/train/core.clj
+++ train/src/train/core.clj
@@ -1,8 +1,9 @@
 (ns train.core)
-(use '[common.dic])
+(require '[common.dic])
 
 (defn -main [& args]
-  (common.dic/init (nth args 0))
-;  (println (common.dic/f "あう"))
-;  (println (common.dic/f "あうi"))
+  (let [dic (common.dic/init (nth args 0))]
+    (println dic)
+    (println ((:find dic) "あう"))
+    )
   )
