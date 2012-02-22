ab3a5a29fb0b8156aea198ec269f1d2ca59e555c
HARUYAMA Seigo <seigo.haruyama@mixi.co.jp>
2012-02-22 20:13:46 +0900

    add ime.common.graph.

---
+++ ime.common/src/ime/common/graph.clj
@@ -0,0 +1,45 @@
+(ns ime.common.graph)
+
+(require '[ime.common.node :as node])
+
+(defn init [dic s]
+  (let [nodes (atom (hash-map))
+        bos (node/init "" "" 0)
+        eos (node/init "" "" (+ 1 (count s)))
+        ]
+    (reset! nodes (assoc @nodes 0 (atom [bos])))
+    (reset! nodes (assoc @nodes (+ 1 (count s)) (atom [eos])))
+
+    (doseq [i (range 0 (count s))]
+      (do
+        (doseq [j (range (+ 1 i) (+ 1 (min (count s) (+ i 16))))]
+          (let [r (subs s i j)]
+            (doseq [w ((dic :find) r)]
+              (let [node (node/init w r j)]
+                (if (contains? @nodes j)
+                  (reset! (get @nodes j) (conj @(get @nodes j) node))
+                  (reset! nodes (assoc @nodes j (atom [node]))))))))
+        (let [r (subs s i (+ 1 i))]
+          (if (not (= r ""))
+            (let [node (node/init r r (+ i 1))]
+              (if (contains? @nodes (+ i 1))
+                (reset! (get @nodes (+ i 1)) (conj @(get @nodes (+ i 1)) node))
+                (reset! nodes (assoc @nodes (+ i 1) (atom [node]))))))))
+      )
+
+
+    (defn get-prevs [node]
+      (cond (node :is_eos?) @(get @nodes (- (node :endpos) 1))
+            (node :is_bos?) []
+            :else @(get @nodes (- (node :endpos) (node :length)))
+            ))
+
+    {:get_nodes (fn [] @nodes)
+     :eos eos
+     :get_prevs get-prevs
+     }
+    )
+  )
+
+
+
---
+++ ime.common/test/ime/common/test/graph.clj
@@ -0,0 +1,25 @@
+(ns ime.common.test.graph
+  (:use [clojure.test]))
+
+(require '[ime.common.node :as node])
+(require '[ime.common.dic :as dic])
+(require '[ime.common.graph :as graph])
+
+(deftest graph
+         (let [dic (dic/init)]
+           ((:add dic) "あう" "あう")
+           ((:add dic) "あう" "会う")
+           ((:add dic) "みんな" "みんな")
+           ((:add dic) "みん" "みん")
+           (let [graph1 (graph/init dic "みんなあう")]
+             (let [eos (graph1 :eos)]
+               (is (= (eos :endpos) 6))
+               (let [prevs_1-1 ((graph1 :get_prevs) eos)]
+                 (is (= (count prevs_1-1) 3))
+                 (is (= (count ((graph1 :get_prevs) (node/init "あう" "あう" 4))) 2))
+                 )
+               )
+             )
+           )
+         )
+
